f8e8f7e5f6c05773a5ac25aa4edc810c4a856fe8
ihorkostrov <lgorkostrow@gmail.com>
2022-04-07 10:55:04 +0300

    added router outlet to parent product component

--- src/app/product/components/product/product.component.ts
+++ src/app/product/components/product/product.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {ProductModel} from "../../models/product.model";
 import {ProductRepository} from "../../services/product.repository";
 import {CartService} from "../../../cart/services/cart.service";
-import {MatSnackBar} from "@angular/material/snack-bar";
 import {SnackbarService} from "../../../core/services/snackbar.service";
 
 @Component({
--- src/app/product/product-routing.module.ts
+++ src/app/product/product-routing.module.ts
@@ -3,14 +3,21 @@ import {NgModule} from "@angular/core";
 import {ProductListPageComponent} from "./components/product-list-page/product-list-page.component";
 import {ProductViewPageComponent} from "./components/product-view-page/product-view-page.component";
 import {resolveProductWithRedirect} from "../shared/guards/product.resolver";
+import {ProductComponent} from "./components/product/product.component";
 
 const routes: Routes = [
   {
-    path: 'products/:productID',
-    component: ProductViewPageComponent,
-    ...resolveProductWithRedirect('/products'),
+    path: 'products',
+    component: ProductComponent,
+    children: [
+      {
+        path: ':productID',
+        component: ProductViewPageComponent,
+        ...resolveProductWithRedirect('/products'),
+      },
+      { path: '', component: ProductListPageComponent },
+    ],
   },
-  { path: 'products', component: ProductListPageComponent },
 ];
 
 @NgModule({
